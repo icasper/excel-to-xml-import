52dcb6daf6beb5e1b697e67db489a773cba5f14a
icasper <ian.casper.za@gmail.com>
2022-02-23 18:25:30 +0200

    updated loop calculation from record length

--- excel-to-xml-import.py
+++ excel-to-xml-import.py
@@ -3,15 +3,10 @@ from xml.dom import minidom
 
 dom = minidom.parse('StationMap.xml')
 stationMap = dom.getElementsByTagName('StationMap')
-for x in range(0,23):
+for x in range(0,stationMap.length):
     print(stationMap[x].toxml())
 
-
-
-
-
-
 dfe = pd.read_excel (r'D:\Projects\python\excel-xml-import\StationCode_Torque Media.xlsx', sheet_name='Torque Media')
-print(dfe.index)
-for x in range(0,245):
+
+for x in range(0,dfe.index.stop):
     print(dfe.loc[x].at['Company'].strip(), dfe.loc[x].at['Code'].strip())
