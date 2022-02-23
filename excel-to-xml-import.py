93b1d7ad5f59a21fdc13fad221fd625c28400683
icasper <ian.casper.za@gmail.com>
2022-02-23 18:01:59 +0200

    Updated  pandas code to strip white space

--- excel-to-xml-import.py
+++ excel-to-xml-import.py
@@ -1,15 +1,17 @@
 import pandas as pd
 from xml.dom import minidom
 
-mydoc = minidom.parse('items.xml')
-
-item = mydoc.getElementsByTagName('item')
-
-
-print(item[1].item.data)
+dom = minidom.parse('StationMap.xml')
+stationMap = dom.getElementsByTagName('StationMap')
+for x in range(0,23):
+    print(stationMap[x].toxml())
 
 
-#dfe = pd.read_excel (r'D:\Projects\python\excel-xml-import\StationCode_Torque Media.xlsx', sheet_name='Torque Media')
-#print(dfe.index)
-#for x in range(0,245):
-#    print(dfe.loc[x].at['Company'], dfe.loc[x].at['Code'])
+
+
+
+
+dfe = pd.read_excel (r'D:\Projects\python\excel-xml-import\StationCode_Torque Media.xlsx', sheet_name='Torque Media')
+print(dfe.index)
+for x in range(0,245):
+    print(dfe.loc[x].at['Company'].strip(), dfe.loc[x].at['Code'].strip())
