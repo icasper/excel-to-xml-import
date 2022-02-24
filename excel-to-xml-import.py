19f42fc4e868384091c1afc9e9d37f72c63c5dd3
icasper <ian.casper.za@gmail.com>
2022-02-24 09:41:26 +0200

    Added pandas itterator for StationData Creation

--- excel-to-xml-import.py
+++ excel-to-xml-import.py
@@ -5,9 +5,9 @@ Creation Date       :   23.2.2022
 Description         :   Helper script to import data
                         from an excel sheet an XML Document
 Change Date         :   24.2.2022
-Change Reason       :   Update pandas code to write out matrix
+Change Reason       :   Update pandas code to write out array
                         from excel to XML file
-                        
+
 """
 # Import the Required modules
 
@@ -22,19 +22,27 @@ print(doc.firstChild.tagName)
 ArrayOfStationMap = doc.getElementsByTagName(doc.firstChild.tagName)
 print(ArrayOfStationMap.length)
 
+# Get Excel sheet into pandas array
+
+dfe = pd.read_excel (r'D:\Projects\python\excel-xml-import\StationCode_Torque Media.xlsx', sheet_name='Torque Media')
+
+# Iterate through pandas array and update each new StationMap created in XML DOM
+
+for x in range(0,dfe.index.stop):
+
 # Creat New StationMap
 
-newStation = doc.createElement('StationMap')
-newBroadcasterId = doc.createElement('BroadcasterId')
-newTelmarId = doc.createElement('TelmarId')
-newLanguage = doc.createElement('Language')
-doc.firstChild.appendChild(newStation)
-newStation.appendChild(newBroadcasterId) 
-newStation.appendChild(newTelmarId)
-newStation.appendChild(newLanguage)
-newBroadcasterId.appendChild(doc.createTextNode('ALGOA'))
-newTelmarId.appendChild(doc.createTextNode('ALG'))
-newLanguage.appendChild(doc.createTextNode('E'))
+    newStation = doc.createElement('StationMap')
+    newBroadcasterId = doc.createElement('BroadcasterId')
+    newTelmarId = doc.createElement('TelmarId')
+    newLanguage = doc.createElement('Language')
+    doc.firstChild.appendChild(newStation)
+    newStation.appendChild(newBroadcasterId) 
+    newStation.appendChild(newTelmarId)
+    newStation.appendChild(newLanguage)
+    newBroadcasterId.appendChild(doc.createTextNode(dfe.loc[x].at['BroadcasterId'].strip()))
+    newTelmarId.appendChild(doc.createTextNode(dfe.loc[x].at['TelmarId'].strip()))
+    newLanguage.appendChild(doc.createTextNode('E'))
 
 # End of New StationMap
 
@@ -47,8 +55,3 @@ print(pretty_xml_as_string)
 
 with open('StationMap.xml','w') as f:
     f.write(pretty_xml_as_string)
-
-#dfe = pd.read_excel (r'D:\Projects\python\excel-xml-import\StationCode_Torque Media.xlsx', sheet_name='Torque Media')
-
-#for x in range(0,dfe.index.stop):
-#    print(dfe.loc[x].at['BroadcasterId'].strip(), dfe.loc[x].at['TelmarId'].strip())
